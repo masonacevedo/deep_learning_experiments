29a34e7f4feb0c901bb901c53aac9a7fe9aa5e8d
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-11 15:28:57 -0700

    even better parameters found!!

--- play.py
+++ play.py
@@ -9,9 +9,9 @@ def function_to_predict(x):
 X_MIN = -5
 X_MAX = 5
 NUM_DATA_POINTS = 100
-NUM_LAYERS = 5
-LAYER_SIZE = 5
-EPOCHS = 100
+NUM_LAYERS = 2
+LAYER_SIZE = 100
+EPOCHS = 1000
 
 x_vals = torch.tensor(np.linspace(X_MIN,X_MAX, NUM_DATA_POINTS), dtype=torch.float32)
 y_vals = function_to_predict(x_vals)
